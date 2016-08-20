44e021d7fb70c563b96c945897a94e5596457b78
Kevin Casado <kscasado@gmail.com>
2016-08-20 14:18:26 -0700

    adding yelp service

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,9 +1,14 @@
-import mongoose from 'mongoose'
-import passport from 'passport'
-import jwt from 'express-jwt'
-var User = mongoose.model('User')
-
+//import mongoose from 'mongoose'
+//import passport from 'passport'
+//import jwt from 'express-jwt'
+//var User = mongoose.model('User')
+import express from 'express'
+var router = express.Router()
+import yelpServ from './yelpService.js'
 module.exports = function(app){
-  var auth = jwt({secret: 'SECRET' , userProperty: 'payload'})
-  app.get('/api/')
+  router.get('/api',function(req,res){
+    console.log('here')
+    console.log(yelpServ.yelpServices.searchYelp('food','Pasadena'))
+    res.send('hi')
+  })
 }
--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 import express from 'express'
-import db from 'mongoose'
+ import db from 'mongoose'
+import yelp from 'yelp'
+import routes from './api/routes.js'
+import yelpServ from './api/yelpService.js'
 const app = express()
 db.connect('mongodb://localhost/plans')
+//require('./api/routes')(app)
+// app.get('*', function (req, res) {
+//   res.send('hello')
+// })
 
-app.get('*', function (req, res) {
-  res.send('hello')
+// app.get('/api/handleYelpOauth',function(req,res) {
+//   console.log(req.query);
+//   res.end(JSON.stringify(req.query, null, 2))
+// })
+app.get('/api/searchYelp',function(req,res){
+  console.log('here')
+  console.log(yelpServ.searchYelp('food','Pasadena'))
+  res.send('hi')
 })
 
 const port = 3000
