c1bb4dd4ae57382fbf0bed382b4d879f19dff504
Kevin Casado <kscasado@gmail.com>
2016-08-23 21:17:35 -0700

    added events to group controller

--- server/api/groups/groupsController.js
+++ server/api/groups/groupsController.js
@@ -1,6 +1,12 @@
 import Group from '../../models/groups'
+import Event from '../../models/events'
 const groupController = { }
 
 groupController.addGroup =function (members, name){
-  group = new GroupSchema 
+  group = new Group(members, name)
+  Group.add(group)
+}
+
+groupController.addEvent = function (event, group){
+  eventAdd = new Event(event)
 }
