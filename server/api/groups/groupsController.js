8a6f5398e5c57950d610dc117e21a42a71a5d881
Kevin Casado <kscasado@gmail.com>
2016-08-24 22:43:37 -0700

    updated syntax for groupController

--- server/api/groups/groupsController.js
+++ server/api/groups/groupsController.js
@@ -2,11 +2,11 @@ import Group from '../../models/groups'
 import Event from '../../models/events'
 const groupController = { }
 
-groupController.addGroup =function (members, name){
-  group = new Group(members, name)
+groupController.addGroup = function (members, name) {
+  var group = new Group (members, name)
   Group.add(group)
 }
 
-groupController.addEvent = function (event, group){
-  eventAdd = new Event(event)
+groupController.addEvent = function (event, group) {
+  var eventAdd = new Event(event)
 }
