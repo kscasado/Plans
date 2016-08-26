f4fb8d30409d7f1ed45103fafb7c5ccaabf3a01a
Kevin Casado <kscasado@gmail.com>
2016-08-25 22:31:41 -0700

    updated searchYelp event

--- client/module/plan/createPlan.jsx
+++ client/module/plan/createPlan.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 
-class CreatePlan extends React.Component {
-  render(){
+const CreatePlan = React.createClass({
+  render() {
     return (
-      <div className='container'>
+      <div className='container-fluid'>
         <div className='row'>
-          <div className='text-center'>
-              <form onSubmit={this.searchYelp}>
-                <h2>Create a Plan</h2>
-                <input placeholder='Enter Search Term' ref='searchTerm' />
-            </form>
-          </div>
+
+              <form className='form-inline' onSubmit={this.searchYelp}>
+                <div className='form-group'>
+                  <label htmlFor='searchTerm' className='control-label'> Enter Search Term </label>
+                  <input className='form-control' placeholder='Enter Search Term' ref='searchTerm' />
+                  <label htmlFor='locationTerm' className='control-label'> Enter Location </label>
+                  <input className='form-control' placeholder='Enter Location' ref='locationTerm' />
+                  <button type="submit" className='btn btn-primary'>Search</button>
+                </div>
+          </form>
+
         </div>
       </div>
     )
+  },
+  searchYelp(event){
+    event.preventDefault()
+    console.log(this.refs)
+    //console.log(searchTerm+location)
   }
-}
+})
 
 module.exports = CreatePlan
