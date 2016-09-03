36da40e9672641c24d250ac4d7461a2437d993b6
Kevin Casado <kscasado@gmail.com>
2016-09-02 23:55:40 -0700

    updated auth.service to have cookie signing

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken'
 import expressJwt from 'express-jwt'
 import config from '../../config/config.json'
 import { User } from '../models'
-
-const EXPIRE = 60*60*5 //5 HOURS
+import compose from 'composable-middleware'
+const EXPIRE = 60 * 60 * 5 // 5 HOURS
 const SECRET = config.secret
 const validateJwt = expressJwt({secret: SECRET})
 /*
@@ -21,7 +21,28 @@ export const accessTokenHeader = (req, res, next) => {
 ** gotten from DB and uses the validateJwt
 */
 const populateReqUser = (req, res, next) => {
-  User.findOne({_id: req.user._id})
-    .then(user)
-
+  User.findOne({_id: req.user._id}, (err, user) => {
+    if (err) {
+      return res.status(401).end()
+    }
+    req.user = user
+    return next()
+  })
+}
+/*
+* Returns middleware function to validate tokens and attach
+* request to user
+*/
+export const isAuthenticated = () => compose()
+  .use(compose().use(accessTokenHeader).user(validateJwt))
+  .use(populateReqUser)
+// Sign Token
+export const signToken = id => jwt.sign({_id: id}, SECRET, {expiresIn: EXPIRE})
+//Sets toekn to the res.cookie
+export const setTokenCookie = (req, res) => {
+  if (!req.user) {
+    return res.status(404).send('It looks like you aren\'t logged in, please try again.')
+  }
+  res.cookie('token', signToken(req.user._id))
+  return res.redirect('/')
 }
