2fa1f8b5d774e12d6d50262cca810562ea23f63d
Kevin Casado <kscasado@gmail.com>
2016-09-03 12:29:53 -0700

    added userComponent in nav

--- client/module/shared/Nav.jsx
+++ client/module/shared/Nav.jsx
@@ -1,26 +1,51 @@
 import React from 'react'
 import cookie from 'react-cookie'
 import jwtDecode from 'jwt-decode'
+import $ from 'jquery'
 
-class Nav extends React.Component {
+const Nav = React.createClass({
   render () {
-    const content = this.props.children
-    // var token = JSON.parse(cookie.load(token))
-    if(cookie.load('token')){
-      console.log(jwtDecode(cookie.load('token')))
+    var userComponent
+    const {user} = this.state
+    if (user) {
+      userComponent= <div>
+        <img src={user.facebook.imageUrl}></img>
+      </div>// insert html here
+    } else {
+      userComponent = <a href='/auth/facebook'>Facebook Login</a>
     }
     return (
 
       <div className='nav navbar-default'>
         <div className='text-right'>
-          <a href='/auth/facebook'>Facebook Login</a>
+          {userComponent}
 
         </div>
 
       </div>
     )
+  },
+  getUserData (_id) {
+    $.ajax({
+      method: 'GET',
+      url: '/api/users/' + _id
+
+    }).done((result) => {
+      this.setState({user: result})
+    })
+  },
+  getInitialState(){
+    return{
+      user: null
+    }
+  },
+  componentWillMount () {
+    if(cookie.load('token')){
+      this.getUserData(jwtDecode(cookie.load('token'))._id)
+    }
   }
-}
-<a href='/auth/facebook'>Facebook Login</a>
+
+
+})
 
 module.exports = Nav
