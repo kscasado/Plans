a76d997e0933ce18c665a939c4c8f9962c845fe9
Kevin Casado <kscasado@gmail.com>
2016-09-03 22:02:21 -0700

    updated plans view

--- client/module/plan/createPlan.jsx
+++ client/module/plan/createPlan.jsx
@@ -10,15 +10,25 @@ const CreatePlan = React.createClass({
 
 
               <form className='form-inline text-center' onSubmit={this._searchYelp}>
-                <div className='form-group'>
-                  <label htmlFor='searchTerm' className='control-label'> Enter Search Term </label>
-                  <input className='form-control' placeholder='Enter Search Term' ref='searchTerm' />
-                  <label htmlFor='locationTerm' className='control-label'> Enter Location </label>
-                  <input className='form-control' placeholder='Enter Location' ref='locationTerm' />
-                  <button type="submit" className='btn btn-primary'>Search</button>
+                <div className='mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>
+                  <label htmlFor='searchTerm' className='mdl-textfield__label'> Enter Search Term </label>
+                  <input className='mdl-textfield__input' placeholder='Enter Search Term' ref='searchTerm' />
+                  </div>
+                  <div className='mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>
+
+                  <label htmlFor='locationTerm' className='mdl-textfield__label'> Enter Location </label>
+                  <input className='mdl-textfield__input' placeholder='Enter Location' ref='locationTerm' />
+                  </div>
+                  <div>
+                  <button type="submit" className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+                    <i className="material-icons">search</i>
+
+                  </button>
                 </div>
           </form>
-          {businesses}
+          <div className='mdl-grid'>
+            {businesses}
+          </div>
         </div>
       </div>
     )
@@ -32,7 +42,6 @@ const CreatePlan = React.createClass({
   },
   createList () {
     const { businesses }= this.state
-    console.log(businesses)
     if(!businesses){
       return<br></br>
     }
@@ -42,18 +51,38 @@ const CreatePlan = React.createClass({
       for(var business of businesses) {
         const businessElement = (
 
-          <div className='panel panel-primary'>
-            <div className='panel-heading'>{business.name}</div>
-            <div className='panel-body yelp-plan'><img src={business.image_url} className='img-responsive'/>
-            <img className='text-right' src={business.rating_img_url} className='img-responsive' />
-            <ul className='list-inline'>
-              {business.categories.map(function(categorie, i){
-                return <li>{categorie[0]} |</li>
-              })}
 
-            </ul>
+          <div className='mdl-card mdl-cell mdl-shadow--4dp'>
+            <div className='mdl-card_media'>
+              <img src={business.image_url} className='img-responsive'/>
+              </div>
+            <div className='mdl-card_title mdl-card--expand'>
+              <a href='/api/yelp/{business.id}'>{business.name}</a>
+            </div>
+
+              <div className='mdl-cad_supporting-text'>
+                <img className='text-right' src={business.rating_img_url} className='img-responsive' />
+                <ul className='list-inline'>
+                  {business.categories.map(function(categorie, i){
+                    return <li>{categorie[0]} |</li>
+                  })}
+
+                </ul>
+            </div>
+            <div className='mdl-layout-spacer'></div>
+          <div className ="mdl-card_menu">
+            <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+              <i className="material-icons">note add</i>
+
+            </button>
+            <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+              <i className="material-icons">share</i>
+
+            </button>
           </div>
-      </div>
+          <br></br>
+          <br></br>
+        </div>
         )
         businessList.push(businessElement)
       }
