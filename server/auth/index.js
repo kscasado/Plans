727cfdc5ed7741f04a9a0d3e8eb882cd05ff8cfd
Kevin Casado <kscasado@gmail.com>
2016-09-22 00:27:00 -0700

    something sillier

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import facebookSetup from './facebook/passport'
 import facebookRouter from './facebook'
-import User from '../models/Users.js'
+import User from '../models/users.js'
 import config from '../../config/config.json'
 facebookSetup(User, config)
 
