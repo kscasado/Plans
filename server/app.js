666365a65781fc1af808e6c2ea69a426a75ac3ed
Kevin Casado <kscasado@gmail.com>
2016-09-22 00:21:56 -0700

    trying something

--- server/app.js
+++ server/app.js
@@ -11,33 +11,36 @@ import webpackconfig from '../webpack.config.js'
 import authRouter from './auth'
 import apiRouter from './api'
 const PORT = Number(process.env.PORT || 3000)
-var isDeveloping = (PORT === 3000) ? true : false
-const compiler = webpack(webpackconfig)
-const middleware = webpackMiddleware(compiler, {
-  publicPath: webpackconfig.output.publicPath,
-  contentBase: 'src',
-  stats: {
-    colors: true,
-    hash: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    modules: false
-  }
-})
-if(isDeveloping) {
-  db.connect('mongodb://localhost/plans')
-} else{
+var isDeveloping = (PORT === 3000)
+
+  const compiler = webpack(webpackconfig)
+
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: webpackconfig.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+    })
+if(isDeveloping){
+    db.connect('mongodb://localhost/plans')
+ }
+else{
   db.connect(config.db.url)
 }
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use('/api', apiRouter)
-app.use('/auth', authRouter)
-app.use(middleware)
-app.use(webpackHotMiddleware(compiler))
-app.get('*', function response (req, res) {
+  const app = express()
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: false}))
+  app.use('/api', apiRouter)
+  app.use('/auth', authRouter)
+  app.use(middleware)
+  app.use(webpackHotMiddleware(compiler))
+  app.get('*', function response (req, res) {
   res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')))
   res.end()
 })
